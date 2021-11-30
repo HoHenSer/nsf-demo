765316a739badaacf8b367653f02abd999071b9d
chenggangyang <1451571361@qq.com>
2021-11-30 20:04:30 +0800

    Update run.sh

--- nsf-demo-stock-advisor/run.sh
+++ nsf-demo-stock-advisor/run.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-java -javaagent:/opt/nsf/nsf-agent.jar=nsf -jar -javaagent:/opt/nsf/skywalking-agent.jar -Dskywalking_config=/opt/nsf/config/agent.config /opt/nsf/nsf-demo-stock-advisor-1.0-SNAPSHOT.jar
+java -javaagent:/opt/nsf/nsf-agent.jar=nsf -javaagent:/opt/npm/napm-java-agent/skywalking-agent.jar -Dskywalking_config=/opt/npm/napm-java-agent/config/agent.config -jar /opt/nsf/nsf-demo-stock-advisor-1.0-SNAPSHOT.jar
